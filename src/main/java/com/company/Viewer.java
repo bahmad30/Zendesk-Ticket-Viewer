6498ac37d85212045104cdde2d7868f23b4f0de7
Bilaal Ahmad <bmahmad2@illinois.edu>
2021-11-26 19:12:36 -0500

    Created methods for getting first, next and previous pages and individual tickets by id.

--- src/main/java/com/company/Viewer.java
+++ src/main/java/com/company/Viewer.java
@@ -15,7 +15,7 @@ import java.net.http.HttpResponse;
 public class Viewer {
 
     // url for requesting from Zendesk API
-    private static final String API_URL = "https://zcctina.zendesk.com/api/v2/tickets";
+    private static final String API_URL = "https://zccbilaal.zendesk.com/api/v2/tickets";
 
     // tickets displayed per page
     private static final int PAGE_SIZE = 25;
@@ -29,7 +29,7 @@ public class Viewer {
     // current page being viewed (one indexed)
     private int page;
 
-    // temporary credentials
+    // TEMPORARY credentials
     private static final String API_AUTH = "bmahmad2@illinois.edu/token:2UM9VcRwPTeOWPJEgsi6FCi7aMqxVsZbGidE6RUX";
 
     /**
@@ -50,27 +50,103 @@ public class Viewer {
 
     /**
      * Requests all tickets for an account.
-     * @return Viewer with populated tickets, meta, links, and page fields.
+     * @return Viewer for first page with populated fields.
      * @throws URISyntaxException exception
      * @throws IOException exception
      * @throws InterruptedException exception
      */
     public static Viewer getTickets() throws URISyntaxException, IOException, InterruptedException {
+        // get first page
+        URI uri = new URI(API_URL + ".json?page[size]=" + PAGE_SIZE);
+        Viewer viewer = getPageHelper(uri);
+        viewer.page = 1;
+
+        return viewer;
+    }
+
+    /**
+     * Requests next page of tickets.
+     * @return Viewer for next page with populated fields. Return current page if next DNE.
+     * @throws IOException exception
+     * @throws InterruptedException exception
+     * @throws URISyntaxException exception
+     */
+    public Viewer getNextPage() throws IOException, InterruptedException, URISyntaxException {
+        // check if no more pages
+        if (!this.meta.isHas_more()) return this;
+
+        // get next page
+        URI uri = new URI(API_URL + ".json?page[size]=" + PAGE_SIZE + "&page[after]=" + meta.getAfter_cursor());
+        Viewer viewer = getPageHelper(uri);
+        viewer.page = this.page + 1;
+
+        // check for case where there are no more tickets despite after_cursor being true
+        if (viewer.tickets.length != 0) return viewer;
+
+        return this;
+    }
+
+    /**
+     * Requests previous page of tickets.
+     * @return Viewer for previous page with populated fields. Return current page if previous DNE.
+     * @throws IOException exception
+     * @throws InterruptedException exception
+     * @throws URISyntaxException exception
+     */
+    public Viewer getPreviousPage() throws URISyntaxException, IOException, InterruptedException {
+        // check if no previous pages
+        if (this.page <= 1) return this;
+
+        // get previous page
+        URI uri = new URI(API_URL + ".json?page[size]=" + PAGE_SIZE + "&page[before]=" + meta.getBefore_cursor());
+        Viewer viewer = getPageHelper(uri);
+        viewer.page = this.page - 1;
+
+        return viewer;
+    }
+
+    /**
+     * Requests individual ticket.
+     * @param id of ticket.
+     * @return Viewer containing array with single ticket.
+     * @throws IOException if ticket with provided id doesn't exist.
+     * @throws URISyntaxException exception
+     * @throws InterruptedException exception
+     */
+    public Viewer getSingleTicket(int id) throws IOException, URISyntaxException, InterruptedException {
+        URI uri = new URI(API_URL + "/show_many.json?ids=" + id);
+        Viewer viewer = getPageHelper(uri);
+        viewer.page = 1;
+
+        // check for non-existent id
+        if (viewer.tickets.length != 0) return viewer;
+
+        throw new IOException();
+    }
+
+    /**
+     * Helper for all get-page methods. Sends requests, handles exceptions if needed.
+     * @param uri URI for specific page.
+     * @return Viewer with populated fields.
+     * @throws IOException exception
+     * @throws InterruptedException exception
+     */
+    private static Viewer getPageHelper(URI uri) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response;
 
         // build request
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(API_URL + ".json?page[size]=" + PAGE_SIZE))
+                .uri(uri)
                 .header("Authorization", "Basic" + API_AUTH)
                 .header("Accept", "application/json")
                 .GET()
                 .build();
 
         // send request and get response. catch and throw exceptions if needed
-        HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+        } catch (Exception e) {
             if (e instanceof IOException) {
                 throw new IOException();
             } else {
@@ -80,23 +156,7 @@ public class Viewer {
 
         // convert json to instance of viewer class (tickets, meta, and links auto-populated)
         ObjectMapper mapper = new ObjectMapper();
-        Viewer viewer = mapper.readValue(response.body(), Viewer.class);
-        viewer.page = 1;
-
-        return viewer;
+        return mapper.readValue(response.body(), Viewer.class);
     }
-
-    public Viewer getNextPage() {
-        return new Viewer();
-    }
-
-    public Viewer getPreviousPage() {
-        return new Viewer();
-    }
-
-    public Viewer getTicketDetails(int id) throws IOException {
-        return new Viewer();
-    }
-
 
 }
